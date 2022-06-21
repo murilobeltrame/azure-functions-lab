58ce8a79dd5552e208e9e8b14c26e2ce356b785a
Murilo Beltrame <murilobeltrame@gmail.com>
2022-06-21 20:33:55 -0300

    updated env

--- environment/terraform/main.tf
+++ environment/terraform/main.tf
@@ -12,10 +12,10 @@ provider "azurerm" {
   features {}
 }
 
-variable "resource_group_name" { }
+variable "resource_group_name" {}
 
 variable "resources_location" {
-  default = "westus2"
+  default = "brazilsouth"
 }
 
 variable "storage_account_name" {
@@ -48,74 +48,68 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "sa" {
-  name = var.storage_account_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location = var.resources_location
-  account_tier = "Standard"
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_container" "sa_container" {
-  name = var.storage_account_container_name
-  storage_account_name = azurerm_storage_account.sa.name
+  name                  = var.storage_account_container_name
+  storage_account_name  = azurerm_storage_account.sa.name
   container_access_type = "private"
 }
 
 resource "azurerm_servicebus_namespace" "sb" {
-  name = var.service_bus_namespace
+  name                = var.service_bus_namespace
   resource_group_name = azurerm_resource_group.rg.name
-  location = var.resources_location
-  sku = "Standard"
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
 }
 
 resource "azurerm_servicebus_topic" "sb_topic_msg_dispatched" {
-  name = "messagesdispatched"
-  resource_group_name = azurerm_resource_group.rg.name
-  namespace_name = azurerm_servicebus_namespace.sb.name
+  name                = "messagesdispatched"
+  namespace_id        = azurerm_servicebus_namespace.sb.id
   default_message_ttl = "P14D"
   auto_delete_on_idle = "P14D"
 }
 
 resource "azurerm_servicebus_subscription" "sb_subscription_msg_dispatched" {
-  name = "MessagesDispatchedSubscription"
-  resource_group_name = azurerm_resource_group.rg.name
-  namespace_name = azurerm_servicebus_namespace.sb.name
-  topic_name = azurerm_servicebus_topic.sb_topic_msg_dispatched.name
-  max_delivery_count = 10
-  default_message_ttl = "P14D"
-  auto_delete_on_idle = "P14D"
+  name                                      = "MessagesDispatchedSubscription"
+  topic_id                                  = azurerm_servicebus_topic.sb_topic_msg_dispatched.id
+  max_delivery_count                        = 10
+  default_message_ttl                       = "P14D"
+  auto_delete_on_idle                       = "P14D"
   dead_lettering_on_filter_evaluation_error = true
-  dead_lettering_on_message_expiration = true
+  dead_lettering_on_message_expiration      = true
 }
 
 resource "azurerm_servicebus_topic" "sb_topic_msg_processed" {
-  name = "messagesprocessed"
-  resource_group_name = azurerm_resource_group.rg.name
-  namespace_name = azurerm_servicebus_namespace.sb.name
+  name                = "messagesprocessed"
+  namespace_id        = azurerm_servicebus_namespace.sb.id
   default_message_ttl = "P14D"
   auto_delete_on_idle = "P14D"
 }
 
 resource "azurerm_servicebus_subscription" "sb_subscription_msg_processed" {
-  name = "MessagesProcessedSubscription"
-  resource_group_name = azurerm_resource_group.rg.name
-  namespace_name = azurerm_servicebus_namespace.sb.name
-  topic_name = azurerm_servicebus_topic.sb_topic_msg_processed.name
-  max_delivery_count = 10
-  default_message_ttl = "P14D"
-  auto_delete_on_idle = "P14D"
+  name                                      = "MessagesProcessedSubscription"
+  topic_id                                  = azurerm_servicebus_topic.sb_topic_msg_processed.id
+  max_delivery_count                        = 10
+  default_message_ttl                       = "P14D"
+  auto_delete_on_idle                       = "P14D"
   dead_lettering_on_filter_evaluation_error = true
-  dead_lettering_on_message_expiration = true
+  dead_lettering_on_message_expiration      = true
 }
 
 resource "azurerm_cosmosdb_account" "dba" {
-  name = var.cosmosdb_account_name
-  location = var.resources_location
+  name                = var.cosmosdb_account_name
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  offer_type = "Standard"
-  kind = "MongoDB"
+  offer_type          = "Standard"
+  kind                = "MongoDB"
   geo_location {
-    location = azurerm_resource_group.rg.location
+    location          = azurerm_resource_group.rg.location
     failover_priority = 0
   }
   consistency_policy {
@@ -124,29 +118,29 @@ resource "azurerm_cosmosdb_account" "dba" {
 }
 
 resource "azurerm_cosmosdb_mongo_database" "db" {
-  name = var.cosmosdb_database_name
+  name                = var.cosmosdb_database_name
   resource_group_name = azurerm_resource_group.rg.name
-  account_name = azurerm_cosmosdb_account.dba.name
+  account_name        = azurerm_cosmosdb_account.dba.name
 }
 
 resource "azurerm_cosmosdb_mongo_collection" "dbc" {
-  name = var.cosmosdb_database_collection_name
+  name                = var.cosmosdb_database_collection_name
   resource_group_name = azurerm_resource_group.rg.name
-  account_name = azurerm_cosmosdb_account.dba.name
-  database_name = azurerm_cosmosdb_mongo_database.db.name
+  account_name        = azurerm_cosmosdb_account.dba.name
+  database_name       = azurerm_cosmosdb_mongo_database.db.name
 }
 
 data "azurerm_servicebus_namespace" "broker" {
-  name = azurerm_servicebus_namespace.sb.name
+  name                = azurerm_servicebus_namespace.sb.name
   resource_group_name = azurerm_resource_group.rg.name
 }
 output "BrokerConnectionString" {
-  value = data.azurerm_servicebus_namespace.broker.default_primary_connection_string
+  value     = data.azurerm_servicebus_namespace.broker.default_primary_connection_string
   sensitive = true
 }
 
 output "StorageConnectionString" {
-  value = azurerm_storage_account.sa.primary_blob_connection_string
+  value     = azurerm_storage_account.sa.primary_blob_connection_string
   sensitive = true
 }
 
@@ -155,11 +149,11 @@ output "StorageContainerName" {
 }
 
 data "azurerm_cosmosdb_account" "database_account" {
-  name = azurerm_cosmosdb_account.dba.name
+  name                = azurerm_cosmosdb_account.dba.name
   resource_group_name = azurerm_resource_group.rg.name
 }
 output "DatabaseConnectionString" {
-  value = "mongodb://${data.azurerm_cosmosdb_account.database_account.name}:${data.azurerm_cosmosdb_account.database_account.primary_key}@${data.azurerm_cosmosdb_account.database_account.name}.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@${data.azurerm_cosmosdb_account.database_account.name}@"
+  value     = "mongodb://${data.azurerm_cosmosdb_account.database_account.name}:${data.azurerm_cosmosdb_account.database_account.primary_key}@${data.azurerm_cosmosdb_account.database_account.name}.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@${data.azurerm_cosmosdb_account.database_account.name}@"
   sensitive = true
 }
 
